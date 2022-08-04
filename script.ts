f4a797adf9add94fc49cf2402751d61c78b4fb23
Muditkumarg <99825033+Muditkumarg@users.noreply.github.com>
2022-08-04 23:43:59 +0530

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,20 @@
+const insert =document.getElementById("insert")
+
+window.addEventListener('keydown',(e) => {
+     if(insert)
+     {insert.innerHTML =
+        ` 
+    <div class="key">
+        ${e.key ===" " ? "Space" : e.key}
+        <small>event.key</small>
+        </div>
+        <div class="key">
+        ${e.keyCode }
+            <small>event.AskeyCode</small>
+        </div>
+       <div class="key"> 
+        ${e.code}
+        <small>event.code</small>
+       </div>
+`;}
+})
